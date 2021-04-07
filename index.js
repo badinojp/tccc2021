c857ec266fe3d0060ccf374e30f747ab9a831b8f
jpsix <juanpablobadino6@gmail.com>
2021-04-07 12:43:06 -0300

    Initial commit

--- index.js
+++ index.js
@@ -9,20 +9,46 @@ const crud = function () {
 
     return {
         create: (name, lastname) => {
-            /*YOUR CODE HERE */
-
+            newItem = {id: Number(), name: '', lastname: ''}
+            if (items.length !== 0) {
+                newItem.id = items[items.length-1].id + 1;
+            } else {
+                newItem.id = 1;
+            }
+            newItem.name = name;
+            newItem.lastname = lastname;
+            items.push(newItem);
+            return newItem;
         },
         read: (id) => {
-            /*YOUR CODE HERE */
-
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].id == id) {
+                    return items[i];
+                } else {
+                    return null;
+                }
+            }
         },
         update: (id, name, lastname) => {
-            /*YOUR CODE HERE */
-
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].id == id) {
+                    items[i].name = name;
+                    items[i].lastname = lastname;
+                    return items[i]
+                } else {
+                    return false
+                }
+            }
         },
         delete: (id) => {
-            /*YOUR CODE HERE */
-
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].id == id) {
+                    delete items[i]
+                    return true
+                } else {
+                    return false
+                }
+            }
         }
     }
 
